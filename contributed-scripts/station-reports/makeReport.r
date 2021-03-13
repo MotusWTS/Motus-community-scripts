ea53ba5a76e9eee485a13145844092349518c218
Lucas Berrigan <lberrigan@bsc-eoc.org>
2021-03-13 09:22:32 -0400

    Add readme and comments. Couple of bug fixes. Plot types changed

--- contributed-scripts/station-reports/makeReport.r
+++ contributed-scripts/station-reports/makeReport.r
@@ -1,65 +1,34 @@
 ##################################################################
-#### 
+####
 #### MOTUS STATION REPORT
 #### Lucas Berrigan
 #### 2 November 2020
-#### 
+####
 ##################################################################
 ####
-#### This script creates a function which produces a station summary for one location.
-####  - It will find all receiver serial numbers for a single 'station'.
+#### ABOUT
+####
+#### This script uses a function to produce a station summary for one location.
+####  - It will find all receiver serial numbers for a single 'station' based on a common name and project ID.
 ####  - It automatically download data from all receivers during that stations history.
 ####  - An option for printable reports removes track hyperlinks (keep false if not printing)
 ####  - To change title and authorship, edit the first two lines of the 'stationReport_v#.#.rmd' document.
-####  - Please report bugs to: lberrigan[AT]birdscanada[DOT]org
-#### 
+####  - Report bugs via GitHub
+####
 ##################################################################
-#### 
+####
 
+# Load the function
+source('func.r')
 
+# Run the function
 makeReport(
-            receiver.name = 'Louisbourg', # The name you want to have displayed on the report
-            receiver.deploy.name = 'Louisbourg', # A character string that is common among all deployment names
+            receiver.name = 'Lawrencetown', # The name you want to have displayed on the report
+            receiver.deploy.name = 'Lawrencetown', # A character string that is common among all deployment names
             projectID = 2, # If NA, will look for matching receiver.deploy.names in all deployments
             printable = F, # Removes hyperlinks, etc.
-            data.dir = 'E:/Data/',
-            save.dir = 'E:/Data/Reports/'
+            data.dir = 'E:/Data/', # Where the Motus data will be stored
+            save.dir = 'E:/Data/Reports/' # Where the reports will be stored
            )
 
-makeReport <- function(receiver.name = '', 
-                       receiver.deploy.name = '', 
-                       projectID = NA, # If NA, will look for matching receiver.deploy.names in all deployments
-                       printable = F, # Removes hyperlinks, etc.
-                       data.dir = 'E:/Data/',
-                       save.dir = 'E:/Data/Reports/'
-                       ) {
-  require(lubridate)
-  require(tidyverse)
-  receiver.name <- receiver.name
-  receiver.deploy.name <- ifelse(is.na(receiver.deploy.name), receiver.name, receiver.deploy.name)
-  language <- 'EN' # English = EN, Francais = FR, Espaniol = ES
- 
-  
-  pdf.fileName <- gsub(' ', '-', paste0(receiver.name, ' Motus Station Report.pdf'), fixed = T)
-  
-  pdf.exists <- file.exists(paste0(save.dir, pdf.fileName))
-  
-  if (!pdf.exists | 
-      (pdf.exists
-    &
-    Sys.time() - file.info(paste0(save.dir, pdf.fileName))$mtime > days(1))) {
-    
-    message(paste0('Starting report for ', receiver.name))
-    
-    
-  # for pdf reports  
-     rmarkdown::render(input = "stationReport_v1.0.rmd", 
-             output_format = "pdf_document",
-             output_file = pdf.fileName,
-             output_dir = save.dir)
-  } else {
-    message(paste0('A report for ', receiver.name, ' has already been made in the past 24 hours'))
-  }
-}
-
-
+# If you have multiple reports to make, save the variables in a data frame and
--- contributed-scripts/station-reports/shiny_app.R
+++ contributed-scripts/station-reports/shiny_app.R
@@ -1,11 +1,35 @@
+################################################
 #
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
+# INSTRUCTIONS
 #
-# Find out more about building applications with Shiny here:
+# Before running this app, you must make the following changes
 #
-#    http://shiny.rstudio.com/
+# 1) Download these two files: "motus-projects.csv" and "receiver-deployments.csv"
+#     - https://motus.org/data/downloads
+# 2) Modify line 21 so it points to where these files are stores. Your Motus data should also be contained here.
+# 3) Download your Motus database using the 'makeReport.r' file if it isn't already.
+# 4) Select the default project ID by modifying line 24
+# 5) Select a default station (by name) by modifying line 27.
+# 6) Set your Google Authentication key (if it already hasn't been set) using line 31. This only has to be done once.
 #
+# Be patient as some plots take time to load!
+#
+
+
+
+# Select a folder where data is stored
+dir <- 'E:/Data/'
+
+# Select a default project
+default.proj <- 2
+
+# Select a default station
+default.recv <- 'Lawrencetown'
+
+# Use the line below to set Google Maps authentication (only needed to be done once)
+library(googleAuthR)
+#options(googleAuthR.client_id = "[ENTER CLIENT ID HERE]", googleAuthR.client_secret = "[ENTER CLIENT SECRET KEY HERE]")
+
 
 library(shiny)
 library(tidyverse)
@@ -17,26 +41,14 @@ library(lubridate)
 library(fuzzyjoin)
 library(oce)
 library(plotly)
-library(googleAuthR)
-
-# Use the line below to set Google Maps authentication (only needed to be done once)
-#options(googleAuthR.client_id = "[ENTER CLIENT ID HERE]", googleAuthR.client_secret = "[ENTER CLIENT SECRET KEY HERE]")
-
 
 Sys.setenv(tz = "GMT")
 
-
-default.proj <- 2
-default.recv <- 'Lawrencetown'
-
-# Select a folder where data is stored
-dir <- 'E:/Data/'
-
-# projected metadata
+# project metadata
 proj.df <- read.csv(paste0(dir, 'motus-projects.csv'))
 
 # Load all receiver deployments
-recv.df <- read.csv(paste0(dir, 'receiver-deployments.csv')) %>% 
+recv.df <- read.csv(paste0(dir, 'receiver-deployments.csv')) %>%
   mutate(tsStart = as.POSIXct(tsStart, origin = '1970-01-01'),
          tsEnd = as.POSIXct(tsEnd, origin = '1970-01-01'),
          dtStart = as.Date(dtStart),
@@ -45,6 +57,8 @@ recv.df <- read.csv(paste0(dir, 'receiver-deployments.csv')) %>%
 projList <- unique(recv.df$recvProjectID)
 
 names(projList) <- proj.df[proj.df$projectID %in% unique(recv.df$recvProjectID),]$projectName
+
+names(projList)[which(is.na(names(projList)))] <- ""
 
 projList <- c("Select one", "All projects", projList)
 
@@ -73,15 +87,15 @@ ui <- fluidPage(
             background-color: #fce5cd;
         }
 
-        body, label, input, button, select { 
+        body, label, input, button, select {
           font-family: "Arial";
         }')
   )),
-  
+
   # Application title
   titlePanel("Motus Station Status"),
-  
-  # Sidebar with a slider input for number of bins 
+
+  # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(id='sidebar',
       selectInput("project", "Select a project", projList, selected = default.proj),
@@ -97,12 +111,13 @@ ui <- fluidPage(
     mainPanel(
       conditionalPanel(condition = "nFiles() > 0",
       tabsetPanel(
-          tabPanel('Devices', 
+          tabPanel('Devices',
                    DT::dataTableOutput('recv_devices'),
                    plotlyOutput('deviceDeployments')
           ),
           tabPanel('Map', plotOutput('recvLocation')),
-          tabPanel('Antennas', 
+          tabPanel('Antennas',
+                   paste('Current antenna position'),
                    tableOutput('antCurrent_tbl'),
                    plotOutput('antHistory_plot'),
                    plotOutput('antHistory_map')
@@ -118,18 +133,18 @@ ui <- fluidPage(
 server <- function(input, output, session) {
   proj <- reactive({input$project})
   recv <- reactive({input$station})
-  
+
   nFiles <- reactive({length(which(file.exists(paste(dir,receiver(),'.motus', sep = ''))))})
-  
+
   output$fileCount <- renderText({
-    
+
     paste0(nFiles(), " of ", length(receiver()), " files exist!")
   #  paste0(nFiles())
-    
+
   })
-  
+
   observe({
-    
+
     # Remove all choices if nothing selected
     if (is.null(proj())) {
       recv.sites <- character(0)
@@ -140,54 +155,54 @@ server <- function(input, output, session) {
     # Update recv input with selections
     updateSelectInput(session, "station",
                       label = "Select a station",
-                      choices = recv.sites, 
+                      choices = recv.sites,
                       selected = default.recv)
   })
     output$recv_devices <- DT::renderDataTable({
-      
-      recv.df %>% 
-        filter(recvProjectID == proj(), siteName == recv()) %>% 
+
+      recv.df %>%
+        filter(recvProjectID == proj(), siteName == recv()) %>%
         select(recvDeployID, receiverID, tsStart, tsEnd) %>%
         mutate_at(c("tsStart", 'tsEnd'), as.POSIXct, origin = '1970-01-01')
-      
+
     })
-    
+
     receiver <- reactive({unique(recv.df[recv.df$recvProjectID == proj() & recv.df$siteName == recv(), ]$receiverID)})
-    
+
     deps.df <- reactive({
-      
+
       message(paste0('Loading receiver deployments for: ', recv()))
-      
+
       df <- bind_rows(lapply(receiver(), function(x){sql.table.to.df(x, 'recvDeps', dir)})) %>%
         mutate(selected = grepl(recv(), name),
                tsStart = as.POSIXct(tsStart, origin = '1970-01-01'),
                tsEnd = ifelse(is.na(tsEnd), Sys.time(), tsEnd),
                tsEnd = as.POSIXct(tsEnd, origin = '1970-01-01'))
       df$name <- factor(df$name, levels = c(unique(filter(df, grepl(recv(), name))$name), unique(filter(df, !grepl(recv(), name))$name)))
-      
+
       message(paste0(nrow(df), ' receiver deployments found.'))
-      
+
       df
     })
-    
-    
+
+
     station.ll <- reactive({
-      
-      deps.df() %>% 
+
+      deps.df() %>%
         filter(selected) %>%
         rename(lat = latitude, lon = longitude) %>%
         summarise(lat = median(lat, na.rm = T), lon = median(lon, na.rm = T)) %>%
         as.list()
-      
+
     })
-    
+
     station.declination  <- reactive({ magneticField(station.ll()$lon, station.ll()$lat, median(deps.df()$tsStart))$declination })
-    
+
     ant.df <- reactive({
       message("ant.df")
-      
+
       message(paste0('Loading antenna deployments for: ', recv()))
-      
+
       df <- bind_rows(lapply(receiver(), function(x){sql.table.to.df(x, 'antDeps', dir)})) %>%
         mutate(bearingMagnetic = round(bearing - station.declination())) %>%
         select(-serno) %>%
@@ -196,11 +211,11 @@ server <- function(input, output, session) {
                depLen = difftime(tsEnd, tsStart, units = 'secs'),
                name = factor(ifelse(selected, recv(), as.character(name))),
                name = factor(name, levels = c(recv(), levels(name)[which(levels(name) != recv())])))
-        
+
         message(paste0(nrow(df), ' antenna deployments found.'))
-      
+
       df
-      
+
     })
     ant.selected.df <- reactive({
       message("ant.selected.df")
@@ -212,60 +227,60 @@ server <- function(input, output, session) {
                plot.lon.lg = longitude + (sin(pi * (-bearing + 90) / 180)*-0.0125),
                plot.lat.lg = latitude + (cos(pi * (-bearing + 90) / 180)*0.0125),
                plot.lon.sm = longitude + (sin(pi * (-bearing + 90) / 180)*-0.0275),
-               plot.lat.sm = latitude + (cos(pi * (-bearing + 90) / 180)*0.0275)) 
-      
+               plot.lat.sm = latitude + (cos(pi * (-bearing + 90) / 180)*0.0275))
+
     })
-    
-    
+
+
     activity.df <- reactive({
-      
+
       message(paste0('Loading antenna activity for: ', recv()))
-      
+
       df <- bind_rows(lapply(receiver(), function(x){sql.table.to.df(x, 'pulseCounts', dir)})) %>%
         mutate(ts = as.POSIXct(as.Date('1970-01-01')) + hours(hourBin),
                date = as.Date(ts),
                ant = factor(ant)) %>%
         filter(as.Date("2010-01-01") < date) %>%
         fuzzy_left_join(
-          select(filter(deps.df(), grepl(recv(), name)), -serno), 
-          by = c('ts' = 'tsStart', 'ts' = 'tsEnd'), 
+          select(filter(deps.df(), grepl(recv(), name)), -serno),
+          by = c('ts' = 'tsStart', 'ts' = 'tsEnd'),
           match_fun = list(`>=`, `<=`)
         ) %>%
         filter(!is.na(deployID))
-      
+
       message(paste0(sum(df$count, na.rm = T), ' antenna pulses found.'))
-      
+
       df
-      
+
     })
-    
+
     gps.df <- reactive({
-      
+
       message(paste0('Loading GPS activity for: ', recv()))
-      
+
       df <- bind_rows(lapply(receiver(), function(x){sql.table.to.df(x, 'gps', dir)})) %>%
         mutate(ts = as.POSIXct(ts, origin = '1970-01-01'),
                date = as.Date(ts)) %>%
         filter(as.Date("2010-01-01") < date) %>%
         fuzzy_left_join(
-          select(filter(deps.df(), grepl(recv(), name)), -serno), 
-          by = c('ts' = 'tsStart', 'ts' = 'tsEnd'), 
+          select(filter(deps.df(), grepl(recv(), name)), -serno),
+          by = c('ts' = 'tsStart', 'ts' = 'tsEnd'),
           match_fun = list(`>=`, `<=`)
         ) %>%
         filter(!is.na(deployID))
-      
+
       message(paste0(nrow(df), ' GPS hits found.'))
-      
+
       df
-      
+
     })
-    
-    
+
+
     map.small <- reactive({
       get_map(location = c(lon = station.ll()$lon, lat = station.ll()$lat), zoom = 10, maptype = "satellite")
     })
-    
-    
+
+
     #######################################################
     #######################################################
     #######################################################
@@ -278,43 +293,43 @@ server <- function(input, output, session) {
     #######################################################
     #######################################################
     #######################################################
-    
+
       output$recvLocation <- renderPlot({
-        
-        
+
+
         message(paste0("Tower coords: ", station.ll()$lat, ", ", station.ll()$lon))
         # Get lat/lon bounding box around these sites
         latLonBounds <- list((station.ll()$lon + c(-1, 1)),
                              (station.ll()$lat + c(-1, 1))
         )
-        
+
         message("nearby.recv.df")
         nearby.recv.df <- recv.df %>%
           filter(latitude > latLonBounds[[2]][1] & latitude < latLonBounds[[2]][2],
                  longitude > latLonBounds[[1]][1] & longitude < latLonBounds[[1]][2],
                  is.na(tsEnd))
-        
+
         message(paste0("Lon: ", range(ant.selected.df()$plot.lon.lg), " - Lat: ", range(ant.selected.df()$plot.lat.lg)))
         ggmap(map.small(), base_layer = ggplot(data = nearby.recv.df, aes(x = longitude, y = latitude)))+
           geom_point(fill = 'red',
                      shape = 21,
-                     stroke = 2, 
-                     size = 2, 
+                     stroke = 2,
+                     size = 2,
                      alpha = 1)+
-          geom_point(aes(x = station.ll()$lon, y = station.ll()$lat), 
+          geom_point(aes(x = station.ll()$lon, y = station.ll()$lat),
                      shape = 8,
                      color = 'black',
                      stroke = 2,
-                     size = 4, 
+                     size = 4,
                      alpha = 1)+
-          geom_point(aes(x = station.ll()$lon, y = station.ll()$lat), 
-                     fill = 'yellow', 
+          geom_point(aes(x = station.ll()$lon, y = station.ll()$lat),
+                     fill = 'yellow',
                      shape = 21,
                      color = 'black',
-                     stroke = 2, 
-                     size = 3, 
+                     stroke = 2,
+                     size = 3,
                      alpha = 1)+
-          geom_text(data = filter(ant.selected.df(), status == 'active'), aes(x = plot.lon.lg, y = plot.lat.lg, angle = - bearing + 90, color = port), 
+          geom_text(data = filter(ant.selected.df(), status == 'active'), aes(x = plot.lon.lg, y = plot.lat.lg, angle = - bearing + 90, color = port),
                     label="\U2192", size = 10)+
           #    coord_fixed(xlim = latLonBounds[[1]], ylim = latLonBounds[[2]])+
           theme(panel.background = element_rect(fill = '#CCDDFF'),
@@ -327,15 +342,15 @@ server <- function(input, output, session) {
           scale_fill_manual(values = c('red' = 'red', 'yellow' = 'yellow'), guide = 'legend', labels = c('Other stations', recv()))
       })
       output$deviceDeployments <- renderPlotly({
-        
+
         n.deps <- length(unique(ant.df()$name))
-        
+
         cust.cols <- c(hcl(seq(from = 0, to = 360 - (360 / n.deps), length.out = n.deps), 100, 75, alpha = c(1, rep(0.1, n.deps - 1))))
-        
+
         serno.selected.deps <- ant.df() %>%
           group_by(serno) %>%
           summarise(selected.start = as.integer(min(tsStart[which(selected)], na.rm = T)))
-        
+
         p <- ant.df() %>%
           left_join(serno.selected.deps, by = 'serno') %>%
           ggplot()+
@@ -343,73 +358,73 @@ server <- function(input, output, session) {
           theme(panel.background = element_rect(fill = NA))+
           scale_fill_manual(values = cust.cols, labels = levels(ant.df()$name))+
           labs(x = 'Time', y = 'Serial Number', fill = '', title = 'Location for each deployment of devices')
-        
+
         ggplotly(p)
-        
+
       })
-      
+
       output$antCurrent_tbl <- renderTable({
-        
-        ant.current.df <- ant.selected.df() %>% 
+
+        ant.current.df <- ant.selected.df() %>%
           filter(deployID == max(ant.selected.df()$deployID)) %>%
           select(port, `Magnetic bearing` = bearingMagnetic, `Height (m)` = heightMeters, `Antenna type` = antennaType)
-        
-        ant.current.df2 <- data.frame(t(ant.current.df[-1])) 
-        
+
+        ant.current.df2 <- data.frame(t(ant.current.df[-1]))
+
         colnames(ant.current.df2) <- paste("Port", ant.current.df$port)
-        
+
         rownames(ant.current.df2) <- c("Bearing (magnetic)", "Height (meters)", "Antenna type")
-        
+
         ant.current.df2
-        
+
       }, rownames = T)
-      
-      
-      
+
+
+
       output$antHistory_plot <- renderPlot({
-        
+
         ant.selected.df() %>%
           ggplot()+
-          geom_rect(aes(xmin = tsStart, xmax = tsEnd, 
-                        ymin = min(heightMeters, na.rm = T) - 0.5, 
+          geom_rect(aes(xmin = tsStart, xmax = tsEnd,
+                        ymin = min(heightMeters, na.rm = T) - 0.5,
                         ymax = max(heightMeters, na.rm = T) + 0.5, fill = factor(deployID)), alpha = 0.1)+
           # Must insert Unicode rather than "→" character to render correctly
-          geom_text(aes(x = tsMean, y = heightMeters, angle = - bearing + 90, color = port), 
+          geom_text(aes(x = tsMean, y = heightMeters, angle = - bearing + 90, color = port),
                     label="\U2192", size = 10)+
           scale_color_manual(values = basic.colours) +
           scale_fill_discrete(guide = 'none') +
           labs(x = '', y = 'Antenna Height (m)', color = 'Port', fill = 'Deployment\nID', title = 'Antenna directions and heights over time')+
           theme(panel.background = element_rect(fill = NA))
-        
+
       })
-      
-      
+
+
       output$antHistory_map <- renderPlot({
         ggmap(map.small(), base_layer = ggplot(data = ant.selected.df()))+
-          geom_text(aes(x = plot.lon.sm, y = plot.lat.sm, angle = - bearing + 90, color = port), 
+          geom_text(aes(x = plot.lon.sm, y = plot.lat.sm, angle = - bearing + 90, color = port),
                     label="\U2192", size = 10)+
           # facet_wrap(~format(tsStart, '%b %Y') + deployID, labeller = label_value)
           facet_wrap(.~interaction(format(tsStart, '%b %Y'), deployID))+
           coord_cartesian()
       })
-      
-      
-      
+
+
+
       output$antActivity_plot <- renderPlotly({
-        
+
         gps.ant.df <- gps.df() %>%
           mutate(ant = factor('GPS', levels = c('GPS', levels(activity.df()$ant))), count = 1) %>%
           select(ts, ant, count) %>%
-          rbind(select(activity.df(), ts, ant, count)) 
-        
+          rbind(select(activity.df(), ts, ant, count))
+
         res <- 0
-        
-      
+
+
         temp.df <- gps.ant.df %>%
           filter(res == 0 | ts > max(ts) - months(res))
-        
+
         if (nrow(temp.df) > 0) {
-          
+
           p <- temp.df %>%
             ggplot(aes(ts, ant, fill = count))+
             geom_tile(aes(width = (difftime(max(ts), min(ts), units = 'days')*100000/365), height = 1))+
@@ -425,11 +440,11 @@ server <- function(input, output, session) {
             gps.ant.gps.df <- gps.ant.df %>%
               filter(res == 0 | ts > max(ts) - months(res)) %>%
               summarise(ts.min = min(ts, na.rm = T), ts.max = max(ts, na.rm = T))
-            
-            p <- p  + 
+
+            p <- p  +
               geom_label(data = gps.ant.gps.df, aes(x = (ts.min + (difftime(ts.max, ts.min, units = 'secs')/2)), y = 'GPS', label  = 'NO GPS DATA EXIST'), fill = 'white')
-          } 
-          
+          }
+
           ggplotly(p)
         } else {
           warning("No GPS hits exist!")
@@ -437,6 +452,5 @@ server <- function(input, output, session) {
       })
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
-
